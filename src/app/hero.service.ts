398622c69977efe13d8e32fa96be953d5efaa933
Akhumzi <lakoyana@gmail.com>
2018-05-10 16:31:56 +0200

    committing error code

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -6,10 +6,7 @@ import { Hero } from './hero';
 import { HEROES } from './mock-heroes';
 import { MessageService } from './message.service';
  
-@Injectable({
-  providedIn: 'root',
-})
-
+@Injectable({ providedIn: 'root' })
 export class HeroService {
  
   constructor(private messageService: MessageService) { }
@@ -19,7 +16,7 @@ export class HeroService {
     this.messageService.add('HeroService: fetched heroes');
     return of(HEROES);
   }
-
+ 
   getHero(id: number): Observable<Hero> {
     // TODO: send the message _after_ fetching the hero
     this.messageService.add(`HeroService: fetched hero id=${id}`);
